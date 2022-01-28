5aaf38164f95ca72cbe1d3fb1f6709553eb165d0
Moviebot1 <98307017+Moviebot1@users.noreply.github.com>
2022-01-29 02:24:56 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -206,7 +206,7 @@ async def auto_filter(bot: Client, update: Message):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"✨Check Links At @HDMOVIESHUB111 ✨ Found {(len_results)} Results For Your Request 😌: <code>{query}</code>",
+                text=f"⚜️Check Links At @HDMOVIESHUB111 ⚜️Found {(len_results)} Results For Your Request 😊: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
